1bf9081a1bfee060c08e854cd033977e229eb67e
MengZe2 <i@mengze2.cn>
2024-12-01 16:57:04 +0800

    更新 process.php

--- public/process.php
+++ public/process.php
@@ -7,8 +7,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_POST['action'];
     $key = $_POST['key'];
     $text = $_POST['text'] ?? '';
-
-    // 初始化加解密工具
     $encryptor = new Encryptor($key);
 
     if ($action === 'encrypt') {
@@ -18,8 +16,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         $result = '无效操作';
     }
-
-    // 返回结果作为 JSON
     header('Content-Type: application/json');
     echo json_encode(['result' => $result]);
 }
